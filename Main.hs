988f91f8b1841160d0cc420cbce7cf43783d349d
Liam McLennan <liam.mclennan@gmail.com>
2013-05-21 22:03:49 +1000

    added analytics

--- Main.hs
+++ Main.hs
@@ -14,6 +14,7 @@ import Evaluator
 main = do
   scotty 3000 $ do
     middleware $ staticPolicy (noDots >-> addBase "public")
+    
     get "/" $ htmlPartial $ W.welcome Start
 
     post "/" $ do
--- Views/Layout.hs
+++ Views/Layout.hs
@@ -34,3 +34,5 @@ template c = docTypeHtml $ do
       H.div ! class_ "nav-collapse" $ ul ! class_ "nav" $ li ! class_ "active" $ a ! href "/" $ "Home"
     H.div ! class_ "container" $ do
       c
+    script "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n\n  ga('create', 'UA-1572176-12', 'shoulditweet.com');\n  ga('send', 'pageview');"
+
